44df58de13157a3286d8f262dec339416893bb40
Aria-iu <2357648739@qq.com>
2024-06-22 15:51:44 +0800

    refine

--- target/debug/build/raspberrypi-OS-2e8f502077f44ca5/build_script_build-2e8f502077f44ca5.d
+++
@@ -1,5 +0,0 @@
-/home/zyc/code/raspberrypi-OS/target/debug/build/raspberrypi-OS-2e8f502077f44ca5/build_script_build-2e8f502077f44ca5: build.rs
-
-/home/zyc/code/raspberrypi-OS/target/debug/build/raspberrypi-OS-2e8f502077f44ca5/build_script_build-2e8f502077f44ca5.d: build.rs
-
-build.rs:
